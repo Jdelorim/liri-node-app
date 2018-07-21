94079dfb2d9087cc31e7d5f1ca52f15748182077
Jdelorim <joshuadelorimier@gmail.com>
2018-07-21 10:24:10 -0400

    just need to fix movie database

--- Liri.js
+++ Liri.js
@@ -4,6 +4,7 @@ var keys = require('./keys');
 var Twitter = require('twitter');
 var Spotify = require('node-spotify-api');
 var request = require('request');
+var fs = require('fs');
 
 var spotify = new Spotify(keys.spotify);
 var client = new Twitter(keys.twitter);
@@ -16,9 +17,9 @@ function begInput(){
     } else if (myprocess === "spotify-this-song") {
         getSong();
     } else if (myprocess === "movie-this"){
-
+        getMovie();
     }else if(myprocess === "do-what-it-says"){
-
+        doit();
     }else{
         console.log("Please enter one of the commands, stupid human");
         console.log("my-tweets");
@@ -39,11 +40,38 @@ function getTweets(){
 
 function getSong(){
     spotify.search({type: 'track', query: songName},function(err,data){
+    var jdata = data.tracks.items;
         if(err) {
             console.log(err);
         } else {
-            console.log(data);
+            console.log("DATA: ",jdata[0].name);
+            console.log("Artist: ",jdata[0].artists[0].name);
+            console.log("Preview: ",jdata[0].preview_url);
+            console.log("Album: ",jdata[0].album.name);
         }
     });
+}
+
+function doit(){
+    fs.readFile("random.txt","UTF-8",function(err,data){
+        console.log(data);
+       var  dataArr = data.split(',');
+       console.log("dataArr: " ,dataArr);
+        var task = dataArr[0];
+        var song = dataArr[1];
+        myprocess = task;
+        songName = song;
+        begInput();
+    })
+}
+
+function getMovie(){
+  request('http://www.omdbapi.com/?t=' + songName + '&apikey=trilogy', function (error, response, body) {
+//  console.log('error:', error); // Print the error if one occurred
+//  console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+  //console.log('body:', JSON.parse(body)); // Print the HTML for the Google homepage.
+  console.log('body:', JSON.parse(body));
+});
+
 }
 begInput();
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # liri-node-app
+
+I used node.js to create a app called Liri-bot. It has four functions. The first one is that is uses the spotify api to collect information on a song title the user enters. The second is it gives information on
